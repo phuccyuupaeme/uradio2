7b5f4fc654b0601f65262fd5a0de2948036c994e
phuccyuupaeme <38471054+phuccyuupaeme@users.noreply.github.com>
2018-12-21 20:09:12 -0600

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,17 @@
+
+<?php
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $Subject = $_POST['subject'];
+    $mailfrom = $_POST['name'];
+    $message = $_POST['message'];
+
+    $mailTo = "kc2suave.com@domainsbyproxy.com";
+    $headers = "From: ".$mailfrom;
+    $txt = "You have received an email form ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: indext.php?mailsend")
+    </p>
+
+}
