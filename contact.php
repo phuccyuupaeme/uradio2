1467838bd7ab100a0f1d5414f40ceef0538e8c34
phuccyuupaeme <38471054+phuccyuupaeme@users.noreply.github.com>
2018-12-27 11:32:56 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,10 +1,9 @@
 <?php
+$to = "kc2suave.com@domainsbyproxy.com";
 $name = $_POST['name'];
 $email = $_POST['email']
 $subject = $_POST['subject'];
 $message = ['message'];
-$to = "kc2suave.com@domainsbyproxy.com";
-$headers = "From: webmaster@example.com" . "\r\n" .
 
-mail($name,$email,$subject,$message,$to,$headers);
+mail($to,$name,$email,$subject,$message);
 ?>
