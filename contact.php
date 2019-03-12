0528345b53ff0fdd850a099ffc6990a7dc3b485c
phuccyuupaeme <38471054+phuccyuupaeme@users.noreply.github.com>
2019-03-12 15:45:57 -0500

    Update and rename contact.php to form-to-email.php

--- contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "kc2suave.com@domainsbyproxy.com";
-$name = $_POST['name'];
-$email = $_POST['email']
-$subject = $_POST['Subject'];
-$message = $_POST['Message'];
-
-mail($to,$name,$email,$subject,$message);
-?>
